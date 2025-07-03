a524db6a008311c57f7633c87ed2d6a855ed0447
Jyothirlatha <perlajyothirlatha0001@gmail.com>
2025-07-03 17:03:25 -0500

    My first commit

---
+++ src/main/java/com/jyothi/contactshareapp/ContactShareAppApplication.java
@@ -0,0 +1,10 @@
+package com.jyothi.contactshareapp;
+
+/**
+ * Hello world!
+ */
+public class ContactShareAppApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
